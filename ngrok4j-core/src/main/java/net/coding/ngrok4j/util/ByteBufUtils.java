eb61625fc948c5fda8eb0459453d8302e685959f
wesl <oncwnuOQP1_2ToE8k4HJTp6YUjFY@git.weixin.qq.com>
2022-06-26 15:35:01 +0800

    update

--- ngrok4j-core/src/main/java/net/coding/ngrok4j/util/ByteBufUtils.java
+++ ngrok4j-core/src/main/java/net/coding/ngrok4j/util/ByteBufUtils.java
@@ -16,29 +16,4 @@ public class ByteBufUtils {
         return buffer;
     }
 
-    public static String byte2hex(byte[] a) {
-        /*StringBuilder sb = new StringBuilder(a.length * 2);
-
-        for (byte b : a)
-            sb.append(String.format("%02x", b & 0xff));
-        return sb.toString();*/
-        String hexString ="";
-        for(int i = 0; i < a.length; i++){
-            String thisByte ="".format("%02x ", a[i]);
-            hexString += thisByte;
-        }
-
-        return hexString;
-
-    }
-
-    public static void main(String[] args) {
-        String json = "{\"Type\":\"Pong\",\"Payload\":{}}";
-        ByteBuf pack = ByteBufUtils.pack(json.getBytes());
-        byte[] data = new byte[pack.readableBytes()];
-        pack.readBytes(data);
-        System.out.println(byte2hex(data));
-
-    }
-
 }
